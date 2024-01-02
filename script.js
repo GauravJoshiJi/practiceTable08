519fba174da14f8ef034b18451c3dd566c62a5d5
GauravJoshiJi <gauravjoshi1997.joshi@gmail.com>
2024-01-02 07:43:28 +0530

    API data into table

--- script.js
+++ script.js
@@ -6,5 +6,17 @@ fetch("https://dummyjson.com/products")
     return objectData.products;
   })
   .then((productData) => {
-    console.log(productData);
+    let products = "";
+    productData.map((values, key) => {
+      products += `
+      <tr>
+      <td>${key + 1}</td>
+      <td>${values.title}</td>
+      <td>${values.description}</td>
+      <td>${values.price}</td>
+      <td>${values.rating}</td>
+      <td><img src="${values.thumbnail}" /></td>
+    </tr>`;
+    });
+    document.getElementById("table_body").innerHTML = products;
   });
