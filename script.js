1931fc2a1ad680601fc6397273f652a76c52e769
GauravJoshiJi <gauravjoshi1997.joshi@gmail.com>
2024-01-02 07:38:59 +0530

    Table creation completed

---
+++ script.js
@@ -0,0 +1,10 @@
+fetch("https://dummyjson.com/products")
+  .then((data) => {
+    return data.json();
+  })
+  .then((objectData) => {
+    return objectData.products;
+  })
+  .then((productData) => {
+    console.log(productData);
+  });
